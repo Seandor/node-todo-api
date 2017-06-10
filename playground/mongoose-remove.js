b87aad9712cf5eb0f5cd4084625c44f44d184671
Seandor <seandor123@gmail.com>
2017-06-10 15:16:34 +0800

    Add delet route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/todo');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('593b999cd194b60a44708418').then((todo) => {
+  console.log(todo);
+});
