eecd250d37be20a35a481b7ef64ada8fe3c1d2bb
Seandor <seandor123@gmail.com>
2017-06-10 16:26:25 +0800

    Add PATCH /todos/:id and test cases

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParse = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -71,8 +72,34 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
-
 });
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  // only let user the update the two properties
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(400).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+})
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
