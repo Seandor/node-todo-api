3fa17769a8c3f468fdfd95256154ae341d3e99fa
Seandor <seandor123@gmail.com>
2018-04-11 21:10:16 +0800

    generate auth token

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -9,6 +11,7 @@ var User = mongoose.model('User', {
     trim: true,
     unique: true,
     validate: {
+      isAsync: false,
       validator: validator.isEmail,
       message: '{VALUE} is not a valid email'
     },
@@ -30,6 +33,26 @@ var User = mongoose.model('User', {
   }]
 });
 
+// overwrite toJSON funtion to not send all the unnecessary data to users
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
 
+  return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  });
+}
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
